0ac573a113325098d30a3a646ba7675085dc568e
derammo <817160+derammo@users.noreply.github.com>
2025-10-26 07:48:30 -0400

    initial

---
+++ __init__.py
@@ -0,0 +1,229 @@
+import bpy
+import re
+import subprocess
+
+def execute_command(self, command):
+    self.report({'INFO'}, "$ " + command)
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    output = result.stdout.strip()
+    error = result.stderr.strip()
+    if output:
+        self.report({'INFO'}, output)
+    if error:
+        self.report({'ERROR'}, error)
+
+def frame_to_time(frame_number, fps, fps_base):
+    # Calculate raw time in seconds
+    raw_time = (frame_number - 1) / (fps / fps_base)
+    
+    # Calculate hours, minutes, seconds, and frames for timecode format
+    hours = int(raw_time / 3600)
+    minutes = int((raw_time % 3600) / 60)
+    seconds = int(raw_time % 60)
+    # frames = int(((raw_time % 1) * (fps / fps_base))) # Remaining fractional part as frames
+    
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+def get_output_dir(self, context):
+    scene = context.scene
+    output_path = scene.render.filepath
+    if not output_path.endswith('/'):
+        output_path = '/'.join(output_path.split('/')[:-1]) + '/'
+    return output_path
+
+class WriteTimestampsOperator(bpy.types.Operator):
+    bl_idname = "derammo.write_timestamps"
+    bl_label = "Write Timestamps for Youtube"
+    bl_description = "Write markers.txt with timeline markers in timecode format for YouTube Description"
+    
+    def execute(self, context):
+        output_dir = get_output_dir(self, context)
+        scene = context.scene
+        fps = scene.render.fps
+        fps_base = scene.render.fps_base
+        markers = scene.timeline_markers
+        sorted_markers = sorted(markers, key=lambda marker: marker.frame)
+
+        marker_path = output_dir + 'markers.txt' 
+        with open(marker_path, 'w') as mf:
+            for marker in sorted_markers:
+                mf.write('{} {}\n'.format(frame_to_time(marker.frame, fps, fps_base), marker.name))
+        return {'FINISHED'}
+
+class RenderAudioFilesOperator(bpy.types.Operator):
+    bl_idname = "derammo.render_audio_files"
+    bl_label = "Render Audio Files"
+    bl_description = "Render audio files between timeline markers in the VSE"
+    
+    def execute(self, context):
+        output_dir = get_output_dir(self, context)
+        scene = context.scene
+        markers = scene.timeline_markers
+        sorted_markers = sorted(markers, key=lambda marker: marker.frame)
+
+        # save rendering settings
+        o_frame_start = scene.frame_start
+        o_frame_end = scene.frame_end
+        o_codec = scene.render.ffmpeg.codec
+        o_render_filepath = scene.render.filepath
+
+        # Configure render settings for audio only
+        scene.render.image_settings.file_format = 'FFMPEG'
+        scene.render.ffmpeg.format = 'MPEG4'  
+        scene.render.ffmpeg.audio_codec = 'AAC'
+        scene.render.ffmpeg.codec =  'NONE'
+
+        track = 1
+        previous = None
+        
+        for marker in sorted_markers:
+            if previous is None:
+                previous = marker
+                continue
+            
+            output_filename = re.sub(r"[^a-zA-Z0-9]", "_", previous.name) + ".aac"
+            scene.frame_start = previous.frame
+            scene.frame_end = marker.frame
+
+            # Set the output file path
+            scene.render.filepath = output_dir + 'audio/' + output_filename
+
+            # Ensure sequencer is enabled for audio rendering
+            scene.sequence_editor_create() # Create a sequence editor if one doesn't exist
+            scene.render.use_sequencer = True
+
+            # Perform the audio mixdown
+            self.report({'INFO'}, "rendering " + scene.render.filepath)
+            bpy.ops.sound.mixdown(filepath=scene.render.filepath, check_existing=True, container='AAC', codec='AAC')
+        
+            # encapsulate as M4A            
+            command = '/opt/homebrew/bin/ffmpeg -y -i ' + scene.render.filepath + ' '
+            
+            # suppress non-error output that ends up in stderr
+            command += '-loglevel error -hide_banner -nostats '
+
+            # album art on all tracks
+            command += '-i ' + output_dir + 'album.png ' \
+                       + '-map 0:0 -map 1:0 '
+
+            command += '-codec copy ' \
+                + '-id3v2_version 3 ' \
+                + '-metadata title="' + previous.name + '" ' \
+                + '-metadata album="' + scene.name + '" ' \
+                + '-metadata track="' + str(track) + '" ' \
+                + '-metadata artist="derammo" ' \
+                + '-metadata genre="Musicals" '
+                
+            # album art on all tracks
+            command += '-disposition:v attached_pic -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" '
+            
+            command += re.sub(r".aac$", ".m4a", scene.render.filepath)
+
+            execute_command(self, command)
+            track += 1
+                
+            # remove AAC
+            execute_command(self, '/usr/bin/trash ' + scene.render.filepath)
+            
+            previous = marker    
+            
+        # restore render settings
+        scene.frame_start = o_frame_start
+        scene.frame_end = o_frame_end
+        scene.render.filepath = o_render_filepath
+        scene.render.ffmpeg.codec = o_codec
+        return {'FINISHED'}
+
+class FillGapsOperator(bpy.types.Operator):
+    bl_idname = "derammo.fill_gaps"
+    bl_label = "Fill Channel Gaps"
+    bl_description = "Fill gaps in channel currently selected strip in the VSE and adjust markers accordingly"
+    
+    def execute(self, context):
+        selected_strips = context.selected_sequences
+        if not selected_strips:
+            self.report({'ERROR'}, "No strips selected.")
+            return {'CANCELLED'}
+        
+        channels_with_selected_strips = set(strip.channel for strip in selected_strips)
+        if len(channels_with_selected_strips) > 1:
+            self.report({'ERROR'}, "More than one channel found. Please select strips from only one channel.")
+            return {'CANCELLED'}
+
+        channel = channels_with_selected_strips.pop()
+        self.report({'INFO'}, f"Channel {channel}:")
+        found = (strip for strip in context.scene.sequence_editor.sequences if strip.channel == channel)
+        strips_in_channel = sorted(found, key=lambda s: s.frame_final_start)
+        previous = None
+        for strip in strips_in_channel:
+            self.report({'DEBUG'}, f"  - {strip.name} (Start: {strip.frame_final_start}, End: {strip.frame_final_end})")
+            if previous is not None:
+                gap = strip.frame_final_start - previous.frame_final_end
+                if gap > 0:
+                    self.report({'INFO'}, f"    Filling gap of {gap} frames before {strip.name}")
+                    text_strip = context.scene.sequence_editor.sequences.new_effect(
+                        name=f"GapFiller_{previous.frame_final_end}_{strip.frame_final_start}",
+                        type='TEXT',
+                        channel=channel,
+                        frame_start=previous.frame_final_end,
+                        frame_end=strip.frame_final_start
+                    )
+                    text_strip.text = "Gap Filler"
+                    for marker in context.scene.timeline_markers:
+                        if previous.frame_final_end <= marker.frame:
+                            marker.frame += gap
+            previous = strip
+        return {'FINISHED'}
+
+    
+class PrintStripsOperator(bpy.types.Operator):
+    bl_idname = "derammo.print_strips"
+    bl_label = "Print Channel Strips"
+    bl_description = "Print strips in any channels with currently selected strips in the VSE"
+
+    def execute(self, context):
+        selected_strips = context.selected_sequences
+        channels_with_selected_strips = set(strip.channel for strip in selected_strips)
+        
+        for channel in sorted(channels_with_selected_strips):
+            self.report({'INFO'}, f"Channel {channel}:")
+            found = (strip for strip in context.scene.sequence_editor.sequences if strip.channel == channel)
+            strips_in_channel = sorted(found, key=lambda s: s.frame_final_start)
+            for strip in strips_in_channel:
+                self.report({'INFO'}, f"  - {strip.name} (Start: {strip.frame_final_start}, End: {strip.frame_final_end})")
+
+        if not selected_strips:
+            self.report({'ERROR'}, "No strips selected.")
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+
+def strip_menu_extension(self, context):
+    self.layout.separator()
+    self.layout.operator(PrintStripsOperator.bl_idname, text="Print Channel Strips")
+    self.layout.operator(FillGapsOperator.bl_idname, text="Fill Channel Gaps")
+
+def render_menu_extension(self, context):
+    self.layout.separator()
+    self.layout.operator(WriteTimestampsOperator.bl_idname, text="Write Timestamps for Youtube")
+    self.layout.operator(RenderAudioFilesOperator.bl_idname, text="Render Audio Files")
+
+def register():
+    bpy.utils.register_class(PrintStripsOperator)
+    bpy.utils.register_class(FillGapsOperator)
+    bpy.utils.register_class(WriteTimestampsOperator)
+    bpy.utils.register_class(RenderAudioFilesOperator)
+    bpy.types.SEQUENCER_MT_strip.append(strip_menu_extension)
+    bpy.types.TOPBAR_MT_render.append(render_menu_extension)
+    print("registered")
+
+def unregister():
+    bpy.types.TOPBAR_MT_render.remove(render_menu_extension)
+    bpy.types.SEQUENCER_MT_strip.remove(strip_menu_extension)
+    bpy.utils.unregister_class(RenderAudioFilesOperator)
+    bpy.utils.unregister_class(WriteTimestampsOperator)
+    bpy.utils.unregister_class(FillGapsOperator)
+    bpy.utils.unregister_class(PrintStripsOperator)
+
+
+
